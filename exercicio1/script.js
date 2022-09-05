64c13033899f03dcc11ad3d905acc0b0fb2e4d32
marllonrp <marllonrp@yahoo.com.br>
2022-09-05 11:35:36 -0300

    Exercícios diários.

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+sacolao = [];
+
+const fruta1 = {
+  nome: "morango",
+  perco: 5,
+  disponibilidade: true,
+};
+
+const fruta2 = {
+  nome: "uva",
+  perco: 12,
+  disponibilidade: false,
+};
+
+const fruta3 = {
+  nome: "melancia",
+  perco: 6,
+  disponibilidade: true,
+};
+
+sacolao.push(fruta1, fruta2, fruta3);
+
+console.log(sacolao);
